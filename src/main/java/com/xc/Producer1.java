ef74cd9093fb36b3d4a078ab9a94da7f24942dba
yuyan <yuyan4991@163.com>
2022-09-02 18:29:31 +0800

    0902

---
+++ src/main/java/com/xc/Producer1.java
@@ -0,0 +1,40 @@
+package com.xc;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+
+public class Producer1 {
+    public static void main(String[] args) throws Exception {
+
+        // 1. 创建 kafka 生产者的配置对象
+        Properties properties = new Properties();
+        // 2. 给 kafka 配置对象添加配置信息：bootstrap.servers
+        //哭了 我又哭了
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9093");
+
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer");
+
+        // 3. 创建 kafka 生产者对象
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<String, String>(properties);
+        System.out.println("开始发送数据");
+             // 4. 调用 send 方法,发送消息
+        for (int i = 0; i < 5; i++) {
+            try {
+                kafkaProducer.send(new ProducerRecord<>("java_test","a111111"+i )).get();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } catch (ExecutionException e) {
+                e.printStackTrace();
+            }
+        }
+        // 5. 关闭资源
+        kafkaProducer.close();
+    }
+
+}
---
+++ src/main/java/com/xc/flink/SqlDemo.java
@@ -0,0 +1,46 @@
+package com.xc.flink;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.table.api.EnvironmentSettings;
+import org.apache.flink.table.api.TableEnvironment;
+import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+
+
+public class SqlDemo {
+    public static void main(String[] args) throws Exception {
+
+        //创建执行环境-TableEnvironment
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        EnvironmentSettings settings = EnvironmentSettings.newInstance()
+                                       .useBlinkPlanner()
+                                       .inStreamingMode() //inBatchMode() --批处理or流处理
+                                       .build();
+        TableEnvironment tableEnv = StreamTableEnvironment.create(env, settings);
+        //在Catlog中创建表
+        //把kafka 中的topic映射成一个输入临时表
+        tableEnv.executeSql(
+                "create table kafka_test (user_id String,user_name String," +
+                        " work_month String,work_amount String) with (" +
+                        "'connector' = 'kafka'," +
+                        "'topic' = 'binlog_data_pipe'," +
+                        "'properties.bootstrap.servers' = 'localhost:9093'," +
+                        "'properties.group.id' = 'test_java_group'," +
+                        "'scan.startup.mode' = 'earliest-offset'," +
+                        "'format' = 'json')");
+
+        String mysql_sql = "CREATE TABLE mysql_sink (\n" +
+                            "user_id     string,\n" +
+                            "user_name   string,\n" +
+                            "work_month  string,\n" +
+                            "work_amount string \n" +
+                            ") WITH (\n" +
+                            "'connector' = 'jdbc',\n" +
+                            "'url' = 'jdbc:mysql://localhost:3306/data_pipe" +
+                            "'table-name' = 'java_kafka_test',\n" +
+                            "'username' = 'root',\n" +
+                            "'password' = '123'\n" +
+                            " )";
+
+        tableEnv.executeSql(mysql_sql);
+//        tableEnv.executeSql("insert into mysql_sink select * from kafka_test");
+    }
+}
